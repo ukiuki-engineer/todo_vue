29eeba6607a6d178b67fd32f3a07e3ee585c91e0
hagemanto-saitama <yuki.hyodo0201@gmail.com>
2022-10-17 05:00:55 +0900

    add:タスク編集機能、備考、備考編集機能追加

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ Vue.component('v-select', VueSelect.VueSelect);
 new Vue({
   el: '#app',
   data: {
+    test: true,
     tasks: [],
     submittedTask: null,
     statuses: [
@@ -13,17 +14,40 @@ new Vue({
       {id: 3, name: '保留'},
       {id: 4, name: '完了'},
     ],
+    subtasks: [
+      {id: 0, name: 'サブタスク1', status_id: 0},
+      {id: 1, name: 'サブタスク2', status_id: 0},
+      {id: 2, name: 'サブタスク3', status_id: 0},
+    ],
+    editting_task_id: null,
+    editting_comment_id: null,
   },
   computed: {
+    hasSubTasks: function() {
+      return function(id) {
+        return id == 0 ? true : false;
+      }
+    },
+    isEditTask: function() {
+      return function(id) {
+        return this.editting_task_id ? id == this.editting_task_id : false;
+      }
+    },
+    isEditComment: function() {
+      return function(id) {
+        return this.editting_comment_id ? id == this.editting_comment_id : false;
+      }
+    },
   },
   methods: {
     // タスクの追加
     addTask: function() {
       this.tasks.push({
         // id: todoStorage.uid++,
-        id: this.tasks.length,
+        id: this.tasks.length + 1,
         name: this.submittedTask,
         status_id: 0,
+        comment: null,
       })
       this.submittedTask = null;
     },
